8c3d3f4daf2ae3dc60be91ee978cb9ea5cb61974
SaishGhadi <108211307+SaishGhadi@users.noreply.github.com>
2024-03-04 18:41:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function add(a,b,res){
+    res=a+b;
+    return res;
+}
+function sub(a,b,res){
+    res=a+b;
+    return res;
+}
+function mul(a,b,res){
+    res=a+b;
+    return res;
+}
+function div(a,b,res){
+    res=a+b;
+    return res;
+}
+
+
+let a=0;
+let b=0;
+let res=0;
